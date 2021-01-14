fb37cc32217d0effd71a936be00b3628e59d6f6b
sunnyraj5555 <headheal2206@gmail.com>
2021-01-14 10:34:31 +0530

    Print Triangle Name

--- Problem8.sh
+++ Problem8.sh
@@ -2,3 +2,15 @@
 read x;
 read y;
 read z;
+
+if [ $x -eq $y ] && [ $y -eq $z ]
+then
+echo "EQUILATERAL";
+
+elif [ $x -ne $y ] && [ $y -ne $z ] && [ $z -ne $x ]
+then
+echo "SCALENE";
+
+else
+echo "ISOSCELES";
+fi
